cbcca99cfa62ddb1f36f627724747426f2c2c5ba
Derik Badman <derik@springshare.com>
2014-03-22 15:48:10 -0400

    fix for modals content duplication

--- js/app/app.js
+++ js/app/app.js
@@ -86,31 +86,8 @@ var AppRouter = Backbone.Router.extend(
 	
 	/**
 	 * Modal utility
-	 * @param {String} title Header for the modal
-	 * @param {String} body Body of the modal
-	 * @param {String} [size] Size class for modal (modal-lg or modal-sm)
+	 * @param {Object} options modal options
 	 */
-	/*
-showModal: function(title, body, buttons, size) {
-		$('#editmodal .modal-title').html(title);
-		$('#editmodal .modal-body').html(body);
-		$('#editmodal .modal-dialog').removeClass('modal-lg modal-sm');
-		if (typeof size !== undefined) {
-			$('#editmodal .modal-dialog').addClass(size);
-		}
-		var modal_footer = '';
-		if (typeof buttons !== undefined && !_.isEmpty(buttons)) {
-			modal_footer += buttons;
-		}
-		modal_footer += ' <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
-		$('#editmodal .modal-footer').html(modal_footer);
-		$('#editmodal').modal({});
-		$('#editmodal').on('shown.bs.modal', function(e) {
-			$(e.target).find('input[type="text"]:first').focus();
-		});
-	},
-*/
-	
 	showModal: function(options) {
 		options = $.extend({
 			title: 'Modal',
@@ -125,7 +102,7 @@ showModal: function(title, body, buttons, size) {
 		}
 		
 		if (options.full_content !== '') {
-			$('#editmodal .modal-content').html(options.full_content);
+			$('#editmodal .modal-content').replaceWith(options.full_content);
 		} else {
 			$('#editmodal .modal-content').empty();
 			$('#editmodal .modal-content').append(this.modalHeader(options.title));
@@ -139,10 +116,20 @@ showModal: function(title, body, buttons, size) {
 		});
 	},
 	
+	/**
+	 * Generate the modal header
+	 * @param {String} title
+	 * @returns {String} the header html
+	 */
 	modalHeader: function(title) {
 		return '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">'+title+'</h4></div>';
 	},
 	
+	/**
+	 * Generate the modal footer
+	 * @param {String} buttons
+	 * @returns {String} the footer html
+	 */
 	modalFooter: function(buttons) {
 		if (typeof buttons == 'undefined') { buttons = ''; }
 		return '<div class="modal-footer">'+buttons+'<button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div>';
@@ -170,14 +157,6 @@ showModal: function(title, body, buttons, size) {
 		$alert.append(text);
 		return $alert;
 	}
-	
-	/*
-loadCustomData: function(){
-		//Couldn't figure out a good way to do this with local files
-		//with a server we could use $.getFile
-		//or use require.js	
-	}
-*/
 	
 });
 
